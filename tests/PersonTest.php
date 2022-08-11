835523ed2a01552be7eda20cfb20f7acbb57eb32
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-08-11 12:06:14 +0700

    Add annotation @before in PersonTest

--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -7,9 +7,17 @@ use Program\Script\Person;
 class PersonTest extends TestCase{
     private Person $person;
 
-    protected function setUp():void{
+    // protected function setUp():void{
+    //     $this->person=new Person("Fajar");
+    // }
+
+    /** 
+     * @before
+    */
+    protected function createPerson(){
         $this->person=new Person("Fajar");
     }
+
     // public function testSuccess(){
     //     $person=new Person("Fajar");
     //     self::assertEquals("Hello Joko, my name is Fajar", $person->sayHello("Joko"));
