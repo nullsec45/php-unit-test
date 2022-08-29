8435a6fd9ba023c62031ab81283d66ebbf4184d9
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-08-29 16:24:05 +0700

    create file ProductRepository

---
+++ src/ProductRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Program\Script;
+
+interface ProductRepository{
+    function save(Product $product):Product;
+    function delete(Product $product):void;
+    function findById(string $id): ?Product;
+    function findAll():array;
+}
