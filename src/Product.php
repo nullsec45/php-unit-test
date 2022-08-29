aecbea08aaff808e208d5680389559a9db0713c8
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-08-29 16:13:21 +0700

    Add setter and getter product

---
+++ src/Product.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Program\Script;
+
+class Product{
+    private string $id, $name, $description;
+    private int $price, $quantity;
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setId(string $id):void{
+        $this->id=$id;
+    }
+    public function getName(){
+        return $this->name;
+    }
+
+    public function setName(string $name):void{
+        $this->name=$name;
+    }
+    public function getDescription(){
+        return $this->description;
+    }
+
+    public function setDescription(string $description):void{
+        $this->description=$description;
+    }
+
+    public function getPrice(int $price):int{
+        return $this->price;
+    }
+
+    public function setPrice(int $price):void{
+        $this->price=$price;
+    }
+    public function getQuantity(int $quantity):int{
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity):void{
+        $this->quantity=$quantity;
+    }
+}
