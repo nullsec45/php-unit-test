a14e5f00c0b323e7c0b360428c9839d3b2928523
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-08-29 16:32:17 +0700

    create file ProductService

---
+++ src/ProductService.php
@@ -0,0 +1,16 @@
+<?php
+namespace Program\Script;
+
+class ProductService{
+    public function __construct(private ProductRepository $repository)
+    {
+        
+    }
+
+    public function register(Product $product):Product{
+        if($this->repository->findById($product->getId())!=null) throw new \Exception("Product is already exists");
+
+        return $this->repository->save($product);
+
+    }
+}
