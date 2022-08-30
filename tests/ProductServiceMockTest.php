c71ac955480d631cc7660d6b890baf46fc20c358
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-08-30 10:49:41 +0700

    quantity

---
+++ tests/ProductServiceMockTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Program\Script;
+
+use PHPUnit\Framework\TestCase;
+
+class ProductServiceTest extends TestCase{
+    private ProductRepository $repository;
+    private ProductService $service;
+
+    protected function setUp():void{
+        $this->repository=$this->createMock(ProductRepository::class);
+        $this->service=new ProductService($this->repository);   
+    }
+
+    public function testMock(){
+        $product=new Product();
+        $product->setId("1");
+
+        $this->repository->excepts(self::once())
+             ->method("findById")
+             ->willReturn($product)
+        $result=$this->repository->findById("1");
+        self::assertSame($product, $result);
+    }
+}
